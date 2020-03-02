a7011c68496ceb87af2807991574cc7beaad8497
Vasyl Shovag <shovagvs@gmail.com>
2020-03-02 09:35:42 +0200

    add docs

---
+++ docs/precache-manifest.0cf4b7b4bf5320c9bcf452af3ce797be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25c12ed35145b129c45126cc41a9c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f2a90ff700c2c57aa04",
+    "url": "/static/css/main.7839f605.chunk.css"
+  },
+  {
+    "revision": "d5ce41292cd93e9bfec0",
+    "url": "/static/js/2.c89fdf0a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c89fdf0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2a90ff700c2c57aa04",
+    "url": "/static/js/main.8dc738f3.chunk.js"
+  },
+  {
+    "revision": "52553d16b2dfafe3c32d",
+    "url": "/static/js/runtime-main.cdc2400c.js"
+  }
+]);
